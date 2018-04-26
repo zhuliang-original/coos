32c8084c9703433c7f50591805937d91e4db96ba
coosx <zhuliang@coos.top>
2018-04-26 17:08:51 +0800

    优化

--- coos.web.core/src/main/java/top/coos/web/core/resource/coos/js/coos.js
+++ coos.web.core/src/main/java/top/coos/web/core/resource/coos/js/coos.js
@@ -7699,7 +7699,7 @@ String.prototype.replaceAll = function(arg1, arg2) {
 			datas[datas.length] = data;
 		});
 		var windowConfig = {};
-		windowConfig.title = "选择" + label;
+		windowConfig.title = "选择" + (label || "");
 		windowConfig.datas = datas;
 		if (ismulti) {
 			if (!co.isEmpty(value)) {
