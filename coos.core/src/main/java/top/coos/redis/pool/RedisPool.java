c48367fc711273b9dffae952544e496f1b720059
coosx <zhuliang@coos.top>
2018-04-27 19:26:43 +0800

    优化

--- coos.core/src/main/java/top/coos/redis/pool/RedisPool.java
+++ coos.core/src/main/java/top/coos/redis/pool/RedisPool.java
@@ -2,6 +2,8 @@ package top.coos.redis.pool;
 
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+import top.coos.tool.string.StringHelper;
 
 /**
  * redis 连接池工具类
@@ -11,21 +13,40 @@ import redis.clients.jedis.JedisPool;
  */
 public final class RedisPool {
 
-	private JedisPool jedisPool = null;
-	private String host;
-	private int port;
-	private Object lock = new Object();
+	private JedisPool pool = null;
+	private final String host;
+	private final String auth;
+	private final int port;
+	private final int timeout = 10000;
+	private final Object lock = new Object();
 
 	public RedisPool(String host, int port) {
 
+		this(host, port, null);
+	}
+
+	public RedisPool(String host, int port, String auth) {
+
 		this.host = host;
 		this.port = port;
+		this.auth = auth;
 		init();
 	}
 
 	private void init() {
 
-		jedisPool = new JedisPool(host, port);
+		JedisPoolConfig config = new JedisPoolConfig();
+		config.setMaxTotal(500);
+		config.setMaxIdle(5);
+		config.setMaxWaitMillis(1000 * 10);
+		// 在borrow一个jedis实例时，是否提前进行validate操作；如果为true，则得到的jedis实例均是可用的；
+		config.setTestOnBorrow(true);
+		// new JedisPool(config, ADDR, PORT, TIMEOUT, AUTH);
+		if (StringHelper.isEmpty(auth)) {
+			pool = new JedisPool(config, host, port, timeout);
+		} else {
+			pool = new JedisPool(config, host, port, timeout, auth);
+		}
 
 	}
 
@@ -37,9 +58,9 @@ public final class RedisPool {
 	public Jedis getClient() {
 
 		try {
-			if (jedisPool != null) {
+			if (pool != null) {
 				synchronized (lock) {
-					Jedis resource = jedisPool.getResource();
+					Jedis resource = pool.getResource();
 					return resource;
 				}
 			} else {
